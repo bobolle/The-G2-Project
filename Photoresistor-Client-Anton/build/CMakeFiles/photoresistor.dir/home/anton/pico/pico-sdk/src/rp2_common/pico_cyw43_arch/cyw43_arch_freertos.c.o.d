5d9ac91eb48ee017a5240dc21318f45591fcf617
pankakan <pankakan@gmail.com>
2025-02-03 15:06:52 +0100

    Added .gitignore and removed build

--- Photoresistor-Client-Anton/build/CMakeFiles/photoresistor.dir/home/anton/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/photoresistor.dir/home/anton/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.o: \
- /home/anton/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c
--- Photoresistor-Client-Anton/build/CMakeFiles/photoresistor.dir/home/anton/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/photoresistor.dir/home/anton/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.o: \
- /home/anton/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
